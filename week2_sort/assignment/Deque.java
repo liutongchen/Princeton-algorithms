c7b168818640098dd8a49b7dbaa729e01b30b15c
liutongchen <liutong.clt@gmail.com>
2018-08-15 14:33:17 -0700

    Create deque interface.

---
+++ week2_sort/assignment/Deque.java
@@ -0,0 +1,72 @@
+/**
+ * Created by Liutong Chen on 08/15/2018
+ */
+
+public class Deque<Item> implements Iterable<Item> {
+
+    /**
+     * Construct an empty deque
+     */
+    public Deque() {
+
+    }
+
+    /**
+     * Check if the deque is empty
+     * @return
+     */
+    public boolean isEmpty() {
+
+    }
+
+    /**
+     * Return the number of items on the deque
+     * @return
+     */
+    public int size() {
+
+    }
+
+    /**
+     * Add the item to the front
+     * @param item
+     */
+    public void addFirst(Item item) {
+
+    }
+
+    /**
+     * Add the item to the last
+     * @param item
+     */
+    public void addLast(Item item) {
+
+    }
+
+    /**
+     * Remove and return the item from the front
+     * @return
+     */
+    public Item removeFirst() {
+
+    }
+
+    /**
+     * Remove and return the item from the end
+     * @return
+     */
+    public Item removeLast() {
+
+    }
+
+    /**
+     * Return an iterator over items in order from front to end
+     * @return
+     */
+    public Iterator<Item> iterator() {
+
+    }
+
+
+    public static void main(String[] args)
+}
