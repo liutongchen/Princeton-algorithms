24020a21c4214a634e3154b61eef7180a9bb5ead
liutongchen <liutong.clt@gmail.com>
2018-08-17 11:50:59 -0700

    Initialize randomizedQueue.

---
+++ week2_stackQueueSort/assignment/RandomizedQueue.java
@@ -0,0 +1,70 @@
+/**
+ * Created by Liutong Chen on 08/17/2018
+ */
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    /**
+     * Construct an empty randomized queue
+     */
+    public RandomizedQueue() {
+
+    }
+
+    /**
+     * Is the randomized queue empty?
+     */
+    public boolean isEmpty() {
+
+    }
+
+    /**
+     * Return the number of items on the randomized queue
+     * @return
+     */
+    public int size() {
+
+    }
+
+    /**
+     * Add the item
+     * @param item
+     */
+    public void enqueue(Item item) {
+
+    }
+
+    /**
+     * Remove and return a random item
+     * @return
+     */
+    public Item dequeue() {
+
+    }
+
+    /**
+     * Return a random item (but do not remove it)
+     * @return
+     */
+    public Item sample() {
+
+    }
+
+    /**
+     * Return an independent iterator over items in random order
+     * @return
+     */
+    public Iterator<Item> iterator() {
+
+    }
+
+    /**
+     * Unit testing (optional)
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
