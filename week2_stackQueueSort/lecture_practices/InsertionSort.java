44a09330f21aa905cb9bd341471f10a418c4fb71
liutongchen <liutong.clt@gmail.com>
2018-08-17 19:48:07 -0700

    Finish insertion sort and write different test cases.

--- week2_stackQueueSort/lecture_practices/InsertionSort.java
+++ week2_stackQueueSort/lecture_practices/InsertionSort.java
@@ -1,4 +1,28 @@
+/**
+ * Create by Liutong Chen on 08/17/2018
+ */
+
+/**
+ * The following implementation takes T((1/4) * n^2) -- quadratic but twice as fast as selection sort.
+ */
 public class InsertionSort {
+
+    /**
+     * Left of i are always sorted;
+     * j is the swap pointer.
+     * @param a
+     */
+    public static void sort(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (less(a[j], a[j - 1])) {
+                    exch(a, j, j-1);
+                }
+                else break;
+            }
+        }
+    }
+
     public static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
--- week2_stackQueueSort/lecture_practices/TestSort.java
+++ week2_stackQueueSort/lecture_practices/TestSort.java
@@ -1,17 +1,60 @@
+/**
+ * Created by Liutong Chen on 08/17/2018
+ */
+
+
+/**
+ * Test results:
+ * Insertion sort works faster than selection sort in partially sorted array;
+ * Selection sort works faster than selection sort in an array of descending order;
+ */
 public class TestSort {
-    public static void main(String[] args) {
-        SelectionSort testSelectionSort = new SelectionSort();
-        System.out.println("-------SELECTION SORT------");
-        Comparable[] arr = {5, 2, 9, 4};
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println("BEFORE: " + arr[i]);
+    private static final Comparable[] randomArr = {5, 7, 3, 2, 4, 10, 1};
+    private static final Comparable[] partiallySortedArr = {2, 3, 4, 9, 5, 6, 7};
+    private static final Comparable[] descendingArr = {7, 6, 5, 4, 3, 2, 1};
+
+    private static Comparable[] copyArray(Comparable[] a) {
+        int len = a.length;
+        Comparable[] copiedArr = new Comparable[len];
+        for (int i = 0; i < len; i++) {
+            copiedArr[i] = a[i];
         }
+        return copiedArr;
+    }
+
+    private static void testDifferentSort(Comparable[] inp) {
+        SelectionSort testSelectionSort = new SelectionSort();
+        InsertionSort testInsertionSort = new InsertionSort();
+
+        System.out.println("-------SELECTION SORT------");
+        Comparable[] SSArrayCopy = copyArray(inp);
         long selectionSortStartTime = System.nanoTime();
-        testSelectionSort.sort(arr);
+        testSelectionSort.sort(SSArrayCopy);
         long selectionSortEndTime = System.nanoTime();
         System.out.println(":::::TIME::::: " + (selectionSortEndTime - selectionSortStartTime));
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println("AFTER: " + arr[i]);
+        for (int i = 0; i <  SSArrayCopy.length; i++) {
+            System.out.println("AFTER: " +  SSArrayCopy[i]);
         }
+
+        System.out.println("-------INSERTION SORT------");
+        Comparable[] ISArrayCopy = copyArray(inp);
+        long insertionSortStartTime = System.nanoTime();
+        testInsertionSort.sort(ISArrayCopy);
+        long insertionSortEndTime = System.nanoTime();
+        System.out.println(":::::TIME::::: " + (insertionSortEndTime - insertionSortStartTime));
+        for (int i = 0; i < ISArrayCopy.length; i++) {
+            System.out.println("AFTER: " + ISArrayCopy[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=======Random Array=========");
+        testDifferentSort(randomArr);
+
+        System.out.println("=======Partially Sorted Array=========");
+        testDifferentSort(partiallySortedArr);
+
+        System.out.println("=======Descending Array=========");
+        testDifferentSort(descendingArr);
     }
 }
