2031c5c3b5d87746831a2b0d69923cde6b57f1b6
liutongchen <liutong.clt@gmail.com>
2018-08-24 17:07:29 -0700

    Initialize BruteCollinearPoints.

---
+++ week3_twoClassicSortingAlgorithms/assignment/BruteCollinearPoints.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Liutong Chen on 08/24/2018
+ */
+
+public class BruteCollinearPoints {
+    /**
+     * Finds all line segments containing 4 points.
+     * To check whether the 4 points p, q, r, and s are collinear, check whether the three slopes between p and q, between p and r, and between p and s are all equal.
+     * @param points
+     */
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    /**
+     * The number of line segments
+     * @return
+     */
+    public int numberOfSegments() {
+
+    }
+
+    /**
+     * The line segments
+     * @return
+     */
+    public LineSegment[] segments() {
+
+    }
+}
