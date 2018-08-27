4d6f053c0ecb9297faefc2a41232039c65894224
liutongchen <liutong.clt@gmail.com>
2018-08-27 09:56:39 -0700

    Stick to no-more-than-four-collinear-points assumption.

--- week3_twoClassicSortingAlgorithms/assignment/BruteCollinearPoints.java
+++ week3_twoClassicSortingAlgorithms/assignment/BruteCollinearPoints.java
@@ -1,10 +1,4 @@
 import java.util.Arrays;
-import java.util.HashMap;
-
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdIn;
-
-import javax.sound.sampled.Line;
 
 /**
  * Created by Liutong Chen on 08/24/2018
@@ -13,7 +7,6 @@ import javax.sound.sampled.Line;
 public class BruteCollinearPoints {
     private int numberOfSegments = 0;
     private LineSegment[] segmentsArr;
-    private HashMap<Double, Point[]> curSegments = new HashMap<>();
 
     /**
      * Finds all line segmentsArr containing 4 points.
@@ -22,6 +15,8 @@ public class BruteCollinearPoints {
      */
     public BruteCollinearPoints(Point[] points) {
         int N = points.length;
+        Arrays.sort(points);
+        segmentsArr = new LineSegment[N];
         for (int point1Id = 0;  point1Id < N; point1Id++) {
             for (int point2Id = point1Id+1; point2Id < N; point2Id++) {
                 for (int point3Id = point2Id + 1; point3Id < N; point3Id++) {
@@ -34,17 +29,8 @@ public class BruteCollinearPoints {
                         double slope1To3 = point1.slopeTo(point3);
                         double slope1To4 = point1.slopeTo(point4);
                         if (slope1To2 == slope1To3 && slope1To2 == slope1To4) {
-                            numberOfSegments++;
-                            Point[] tempPointArr = new Point[]{point1, point2, point3, point4};
-                            Arrays.sort(tempPointArr);
-                            Point[] prevPoints = curSegments.get(slope1To2);
-                            if (prevPoints != null) {
-                                tempPointArr[1] = prevPoints[0];
-                                tempPointArr[2] = prevPoints[1];
-                                Arrays.sort(tempPointArr);
-                            }
-                            curSegments.put(slope1To2, new Point[]{tempPointArr[0], tempPointArr[3]});
-                        }
+                            segmentsArr[numberOfSegments++] = new LineSegment(point1, point4);
+                       }
                     }
                 }
             }
@@ -64,33 +50,6 @@ public class BruteCollinearPoints {
      * @return
      */
     public LineSegment[] segments() {
-        segmentsArr = new LineSegment[curSegments.size()];
-        int iter = 0;
-        for (Point[] endPoints : curSegments.values()) {
-            segmentsArr[iter++] = new LineSegment(endPoints[0], endPoints[1]);
-        }
-        return segmentsArr;
-    }
-
-    /**
-     * For testing purpose, delete later
-     * TODO: DELETE THIS BEFORE SUBMISSION
-     */
-    public static void main(String[] args) {
-        Point[] points = new Point[]{
-                new Point(19000, 10000),
-                new Point(18000, 10000),
-                new Point(32000, 10000),
-                new Point(21000, 10000),
-                new Point(1234, 5678),
-                new Point(14000, 10000),
-        };
-
-        BruteCollinearPoints testBrute = new BruteCollinearPoints(points);
-        System.out.println(testBrute.numberOfSegments());
-        LineSegment[] lineSegments = testBrute.segments();
-        for (LineSegment line: lineSegments) {
-            System.out.println(line);
-        }
+        return Arrays.copyOfRange(segmentsArr, 0, numberOfSegments);
     }
 }
