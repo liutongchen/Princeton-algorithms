57d4095cb97e70679ddde8e8a93bd749ea147fec
liutongchen <liutong.clt@gmail.com>
2018-08-27 09:59:26 -0700

    Initialize FastCollinearPoints.

---
+++ week3_twoClassicSortingAlgorithms/assignment/FastCollinearPoints.java
@@ -0,0 +1,25 @@
+public class FastCollinearPoints {
+    /**
+     * Finds all line segments containing 4 or more points
+     * @param points
+     */
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+    /**
+     * The number of line segments
+     * @return
+     */
+    public int numberOfSegments() {
+
+    }
+
+    /**
+     * The line segments
+     * @return
+     */
+    public LineSegment[] segments() {
+
+    }
+}
