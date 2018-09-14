658eea7ac56b9d86311ed3d7b1c2319a0dd1b8b1
liutongchen <liutong.clt@gmail.com>
2018-09-13 21:49:12 -0700

    Initialize Board class.

---
+++ week4_PQAndElementarySymbolTables/assignment/Board.java
@@ -0,0 +1,83 @@
+/**
+ * Created by Liutong Chen on 09/13/2018
+ */
+
+public class Board {
+    /**
+     * Construct a board from an n-by-n array of blocks (where blocks[i][j] = block in row i, column j)
+     * @param blocks
+     */
+    public Board(int[][] blocks) {
+
+    }          
+
+    /**
+     * Board dimension n
+     * @return
+     */
+    public int dimension() {
+
+    }
+
+    /**
+     * Number of blocks out of place
+     * @return
+     */ 
+    public int hamming() {
+
+    }
+
+    /**
+     * Sum of Manhattan distances between blocks and goal
+     * @return
+     */
+    public int manhattan() {
+
+    }
+
+    /**
+     * Is this board the goal board?
+     * @return
+     */
+    public boolean isGoal() {
+
+    }
+
+    /**
+     * A board that is obtained by exchanging any pair of blocks
+     * @return
+     */
+    public Board twin() {
+
+    }
+
+    /**
+     * Does this board equal y?
+     */
+    public boolean equals(Object y) {
+
+    } 
+
+    /**
+     * All neighboring boards
+     * @return
+     */
+    public Iterable<Board> neighbors() {
+
+    }
+
+    /**
+     * String representation of this board (in the output format specified below)
+     */
+    public String toString() {
+
+    }
+
+    /**
+     * Unit tests (not graded)
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+}
