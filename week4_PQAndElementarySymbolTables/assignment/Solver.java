c9f829572c8653e56f47c7439833f78cce801f88
liutongchen <liutong.clt@gmail.com>
2018-09-13 21:54:58 -0700

    Initialize Solver class.

---
+++ week4_PQAndElementarySymbolTables/assignment/Solver.java
@@ -0,0 +1,46 @@
+/**
+ * Created by Liutong Chen on 09/13/2018
+ */
+
+public class Solver {
+
+    /**
+     * Find a solution to the initial board (using the A* algorithm)
+     * @param initial
+     */
+    public Solver(Board initial) {
+
+    }
+
+    /**
+     * Is the initial board solvable?
+     * @param initial
+     */
+    public boolean isSolvable() {
+
+    }            
+
+    /**
+     * Min number of moves to solve initial board; -1 if unsolvable
+     * @param initial
+     */
+    public int moves() {
+        
+    }                     
+
+    /**
+     * Sequence of boards in a shortest solution; null if unsolvable
+     * @param initial
+     */
+    public Iterable<Board> solution() {
+
+    }     
+
+    /**
+     * Solve a slider puzzle (given below)
+     * @param initial
+     */
+    public static void main(String[] args) {
+        
+    }
+}
