8696abb38e32d5ff606b4d5c924bb6467582a2fe
Liutong Chen <liutong.clt@gmail.com>
2018-09-17 16:10:21 -0700

    Write README.

---
+++ week4_PQAndElementarySymbolTables/assignment/README.md
@@ -0,0 +1,17 @@
+Write a program to solve the 8-puzzle problem (and its natural generalizations) using the A* search algorithm.
+http://coursera.cs.princeton.edu/algs4/assignments/8puzzle.html
+
+## Board
+
+**Corner cases:**
+You may assume that the constructor receives an n-by-n array containing the n2 integers between 0 and n2 − 1, where 0 represents the blank square.
+
+**Performance Requirement:**
+Your implementation should support all Board methods in time proportional to n2 (or better) in the worst case.
+
+## Solver 
+To implement the A* algorithm, you must use MinPQ from algs4.jar for the priority queue(s).
+
+
+## Score
+
--- week4_PQAndElementarySymbolTables/assignment/Solver.java
+++ week4_PQAndElementarySymbolTables/assignment/Solver.java
@@ -1,20 +1,40 @@
 /**
  * Created by Liutong Chen on 09/13/2018
  */
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Iterator;
 
 public class Solver {
+    private MinPQ solutionPQ;
+    private int moves = 0;
+    private class Move implements Comparable<Move> {
+        private Board board;
+        public Move(Board board) {
+            this.board = board;
 
+        }
+        public int compareTo(Move move) {
+
+        }
+    }
     /**
      * Find a solution to the initial board (using the A* algorithm)
      * @param initial
      */
     public Solver(Board initial) {
-
+        // Create new Move with initial board
+        // add initial move to MinPQ
+        // if initial move is the goal: return moves = 0
+        // else:
+        // add all neighbors of initial moves to the PQ if not exist before
+        // loop: delMin() -- bestMove from PQ and check bestMove
     }
 
     /**
      * Is the initial board solvable?
-     * @param initial
      */
     public boolean isSolvable() {
 
@@ -22,7 +42,6 @@ public class Solver {
 
     /**
      * Min number of moves to solve initial board; -1 if unsolvable
-     * @param initial
      */
     public int moves() {
         
@@ -30,17 +49,35 @@ public class Solver {
 
     /**
      * Sequence of boards in a shortest solution; null if unsolvable
-     * @param initial
      */
     public Iterable<Board> solution() {
 
-    }     
+    }
 
     /**
      * Solve a slider puzzle (given below)
-     * @param initial
      */
     public static void main(String[] args) {
-        
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
     }
 }
